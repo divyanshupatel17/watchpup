12d336456c1bde5552b4478373b1958cc480ef91
varshith pilli <varshith6946@gmail.com>
2026-02-12 20:14:57 +0530

    feat: automate sem_id

--- README.md
+++ README.md
@@ -19,12 +19,26 @@ Install dependencies:
 pip install -r requirements.txt
 ```
 
+env variables:
+```
+REGD=xxxxxxxxxxxx
+PASS=xxxxxxxxxxxx
+
+VTOP_SEMID=xxxxxxxxxxxxxx
+INTERVAL_SECONDS =xxxxxxxxxxx # in seconds
+MAX_RETIRES=10 # to skip the google captcha
+
+TG_BOT_TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxx
+TG_CHAT_ID=xxxxxxxxxxxxxxxxxxxx
+```
+You could get the `VTOP_SEMID` from the list by running get_semid.py
+
 TODO:
  - [X] show actual change in the data
  - [X] logging
  - [X] automatic cookie fetching (gotta deal with captcha)
  - [X] notifying the user
- - [ ] extend to more than just the marks page
- - [ ] automatic sem id maybe?
+ - [X] automatic sem id maybe
+ - [ ] extend to more than just the marks page (~~grades~~, ~~cgpa~~, calendar)
  - [ ] error handlers
  - [ ] maybe make an exe
---
+++ get_semid.py
@@ -0,0 +1,48 @@
+from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
+
+from handlers.auth import get_csrf_auth
+
+load_dotenv()
+
+REGD = os.getenv("REGD")
+PASS = os.getenv("PASS")
+
+CSRF, session = get_csrf_auth(REGD, PASS)
+
+url = "https://vtopcc.vit.ac.in/vtop/academics/common/StudentTimeTableChn"
+
+headers = {
+    "Origin": "https://vtopcc.vit.ac.in",
+    "Referer": "https://vtopcc.vit.ac.in/vtop/content",
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0",
+}
+
+data = {
+    "authorizedID": REGD,
+    "verifyMenu": "true",
+    "_csrf": CSRF,
+}
+
+response = session.post(
+    url,
+    headers=headers,
+    data=data,
+    timeout=30,
+    verify=False
+)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+select = soup.find("select", id="semesterSubId")
+
+for opt in select.find_all("option"):
+    sem_id = opt.get("value", "").strip()
+    sem_name = opt.get_text(strip=True)
+
+    if not sem_id:
+        continue
+
+    print(sem_id, "->", sem_name)
